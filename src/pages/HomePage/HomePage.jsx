6868f1a8d88a1fb714dad9195f626e9ff441a269
dymafr <contact@dyma.fr>
2023-11-02 23:55:51 +0100

    Initial commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import styles from './HomePage.module.scss';
+import Recipe from './components/Recipe/Recipe';
+import Loading from '../../components/Loading/Loading';
+
+export default function HomePage() {
+  const [recipes, ] = useState([]);
+  const [isLoading, ] = useState(true);
+  const [filter, setFilter] = useState('');
+
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
+
+  return (
+    <div className="flex-fill container d-flex flex-column p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div
+        className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
+      >
+        <div
+          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <i className="fa-solid fa-magnifying-glass mr-15"></i>
+          <input
+            onInput={handleInput}
+            className="flex-fill"
+            type="text"
+            placeholder="Rechercher"
+          />
+        </div>
+        {isLoading && !recipes.length ? (
+          <Loading />
+        ) : (
+          <div className={styles.grid}>
+            {recipes
+              .filter((r) => r.title.toLowerCase().startsWith(filter))
+              .map((r) => (
+                <Recipe key={r._id} title={r.title} image={r.image} />
+              ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+ 
